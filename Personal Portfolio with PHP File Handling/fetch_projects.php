48431a197020c4154c6198e4ad7e937ae205be8b
Masashidot <galaxybolt56@gmail.com>
2024-12-12 02:34:23 +0900

    Delete Personal Portfolio with PHP File Handling directory

--- Personal Portfolio with PHP File Handling/fetch_projects.php
+++
@@ -1,9 +0,0 @@
-<?php
-$projectsFile = 'projects.json';
-if (file_exists($projectsFile)) {
-    $projects = json_decode(file_get_contents($projectsFile), true);
-    echo json_encode(['success' => true, 'projects' => $projects]);
-} else {
-    echo json_encode(['success' => true, 'projects' => []]);
-}
-?>
